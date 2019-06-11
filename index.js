9d46c561a3857d92a6721590fcbf1165cecc53ee
valeria campilongo <cvaleriac@hotmail.it>
2019-06-11 12:24:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,72 +5,52 @@ let mealId = 0;
 let customerId = 0;
 let deliveryId = 0;
 
-  class Neighborhood {
-      constructor(name) {
-          this.name = name;
-          this.id = ++neighborhoodId;
-          store.neighborhoods.push(this);
-      }
-      deliveries() {
-          return store.deliveries.filter(
-              function(delivery) {
-                  return delivery.neighborhoodId === this.id;
-              }.bind(this)
-          );
-      }
-      customers() {
-          return store.customers.filter(
-              function(customer) {
-                  return customer.neighborhoodId === this.neighborhoodId;
-              }.bind(this)
-          );
-      }
-  }
-
-    class Customer{
-        constructor(name) {
-            this.name = name;
-            this.id = ++customerId;
-            this.neighborhoodId = neighborhoodId;
-            store.customers.push(this);
+class Neighborhood {
+    constructor(name) {
+        this.name = name;
+        this.id = ++neighborhoodId;
+        store.neighborhoods.push(this);
     }
+    
     deliveries() {
         return store.deliveries.filter(
             function(delivery) {
-                return delivery.customerdId === this.id;
+                return delivery.neighborhoodId == this.id;
             }.bind(this)
         );
     }
-
+    customers() {
+        return store.customers.filter(
+            function(customer) {
+                return customer.neighborhoodId === this.id;
+            }.bind(this)
+        );
+    }
     meals() {
-        return store.meals.filter(
-            function(delivery) {
-                return delivery.customerId === this.id;
-            }.bind(this)
-        );
+        let neighborhoodMeals = this.deliveries().map(
+            function(delivery){
+                return delivery.meal()
+            }
+        )
+        return [...new Set(neighborhoodMeals)];
     }
+}
 
-    totalSpent() {
-          return this.meals().reduce((total, meal) => (total += meal.price), 0);
-      }
-    }
 
-    class Meals {
-      constructor(title, price){
-        this.title = name;
-        this.id = ++mealId;
+class Meal {
+    constructor(title, price) {
+        this.title = title;
         this.price = price;
+        this.id = ++mealId;
         store.meals.push(this);
     }
-
     deliveries() {
         return store.deliveries.filter(
             function(delivery) {
-                return delivery.mealId === this.id;
+                return delivery.mealId == this.id;
             }.bind(this)
         );
     }
-
     customers() {
         return this.deliveries().map(delivery => {
           return delivery.customer()
@@ -80,5 +60,64 @@ let deliveryId = 0;
         return store.meals.sort(function (a, b) {
             return b['price'] - a['price'];
         });
+    }
+}
+
+
+class Customer {
+    constructor(name, neighborhoodId) {
+        this.name = name;
+        this.id = ++customerId;
+        this.neighborhoodId = neighborhoodId;
+        store.customers.push(this);
+    }
+    deliveries() {
+        return store.deliveries.filter(
+            function(delivery) {
+                return delivery.customerId === this.id;
+            }.bind(this)
+        );
+    }
+    meals() {
+        return this.deliveries().map (
+            function(delivery) {
+                return delivery.meal()
+            }
+        );
+    };
+    totalSpent() {
+        return this.meals().reduce((total, meal) => (total += meal.price), 0);
+    }
+}
+
+
+class Delivery {
+    constructor(mealId, neighborhoodId, customerId) {
+        this.id = ++deliveryId;
+        this.neighborhoodId = neighborhoodId;
+        this.customerId = customerId;
+        this.mealId = mealId;
+        store.deliveries.push(this);
+    }
+    neighborhood() {
+        return store.neighborhoods.find(
+            function(neighborhood) {
+                return neighborhood.id === this.neighborhoodId;
+            }.bind(this)
+        );
+    }
+    customer() {
+        return store.customers.find(
+            function(customer) {
+                return customer.id === this.customerId;
+            }.bind(this)
+        );
+    }
+    meal() {
+        return store.meals.find(
+            function(meal) {
+                return meal.id === this.mealId;
+            }.bind(this)
+        );
     }
 }
