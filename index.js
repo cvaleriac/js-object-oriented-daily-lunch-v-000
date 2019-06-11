a0c1b4b101e1fa9d3ab74036e6beb85fd114b7fb
valeria campilongo <cvaleriac@hotmail.it>
2019-06-11 12:02:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId = 0;
+let mealId = 0;
+let customerId = 0;
+let deliveryId = 0;
+
+class Neighborhood {
+    constructor(name) {
+        this.name = name;
+        this.id = ++neighborhoodId;
+        store.neighborhoods.push(this);
+    }
+    deliveries() {
+        return store.deliveries.filter(
+            function(delivery) {
+                return delivery.neighborhoodId === this.id;
+            }.bind(this)
+        );
+    }
+    customers() {
+        return store.customers.filter(
+            function(customer) {
+                return customer.neighborhoodId === this.neighborhoodId;
+            }.bind(this)
+        );
+    }
+}
+
+class Customer{
+    constructor(name) {
+        this.name = name;
+        this.id = ++customerId;
+        this.neighborhoodId = neighborhoodId;
+        store.customers.push(this);
+}
+deliveries() {
+    return store.deliveries.filter(
+        function(delivery) {
+            return delivery.customerdId === this.id;
+        }.bind(this)
+    );
+}
+
+meals() {
+    return store.meals.filter(
+        function(delivery) {
+            return delivery.customerId === this.id;
+        }.bind(this)
+    );
+}
+
+totalSpent() {
+      return this.meals().reduce((total, meal) => (total += meal.price), 0);
+  }
+}
